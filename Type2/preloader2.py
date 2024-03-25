c96329db64bae23fbad6e389dc31eccd5abeab0a
Amirhossein ilkhani <128811375+Amirhossein-ilkhani@users.noreply.github.com>
2024-03-25 23:09:46 +0330

    loading dataset

--- Type2/preloader2.py
+++ Type2/preloader2.py
@@ -5,7 +5,7 @@ from multiprocessing import Pool
 
 def process_data(args):
     i, j, df_candidates, df_inputs = args
-    # print(i,j+i)
+    print(i,j+i)
     uid_new = df_inputs.iloc[i, 0] + " " +  df_inputs.iloc[i+j, 0]
     uid_new_inverse = df_inputs.iloc[i+j, 0] + " " +  df_inputs.iloc[i, 0]
     if(((df_candidates.iloc[:,:] == uid_new).any()).bool() or ((df_candidates.iloc[:,:] == uid_new_inverse).any()).bool()):
@@ -15,8 +15,8 @@ def process_data(args):
     return [uid_new , df_inputs.iloc[i, 1], df_inputs.iloc[i, 2], df_inputs.iloc[i+j, 1], df_inputs.iloc[i+j, 2], label]
 
 if __name__ == "__main__":
-    df_candidates = pd.read_csv("Type2/candidates.csv")
-    df_inputs = pd.read_csv("Type2/Inputs.csv")
+    df_candidates = pd.read_csv("candidates.csv")
+    df_inputs = pd.read_csv("Inputs.csv")
     uid = df_inputs.iloc[: , 0]
 
     df = pd.DataFrame(columns=['uid', 'vbm_vac1', 'cbm_vac1', 'vbm_vac2', 'cbm_vac2', 'label_type_2'])
@@ -32,5 +32,5 @@ if __name__ == "__main__":
     for result in results:
         df.loc[len(df)] = result
 
+    df.to_csv('Dataset1.csv')
     print("finished")
-    df.to_csv('Type2/Dataset.csv')
